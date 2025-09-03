ec19e5bb8e8bc9ac2575333f55693ed22cdd9250
woongcheol Na <32201345@dankook.ac.kr>
2025-09-03 17:29:20 +0900

    애너테이션 사용 연습 - 생성자 관련

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -11,21 +11,26 @@ import lombok.*;
 @ToString
 @Entity //-> JPA가 이 어노테이션을 보고 해당 클래스와 닮은 테이블을 만들어준다
 //@AllArgsConstructor
-@RequiredArgsConstructor // 고정이 필요한 필드 변수에 final 키워드를 붙이면 생성자로 만들어준다
+//@RequiredArgsConstructor // 고정이 필요한 필드 변수에 final 키워드를 붙이면 생성자로 만들어준다
+@NoArgsConstructor // 특수한 생성자를 만들더라도 기본생성자를 따로 만들어준다
 public class Post {
     @Id // -> JPA야 id를 PK로 삼고
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY) // -> JPA야 autoIncrement를 걸어라
     private int id;
-    private final String title;
+    private String title;
     @Column(columnDefinition = "TEXT")
-    private final String content;
+    private String content;
 
     //public Post() {} // 아래와 같이 특수한 생성자를 만들면 자바가 더이상 기본 생성자를 따로 만들어주지 않기 때문에
 
     // 이렇게 직접 생성자를 정의하지 않고 @AllArgsConstructor을 이용해서 자동으로 생성할 수도 있다
-    public Post() {
-        this.title = "고정된 값";
-        this.content = "고정된 값";
+//    public Post() {
+//        this.title = "고정된 값";
+//        this.content = "고정된 값";
+//    }
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 
 }
